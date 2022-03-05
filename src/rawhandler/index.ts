ca2697a468deea9a94ab44a9d162ded079d7dd7a
gustavoSouzavs <gustavo.araujo@aluno.uepa.br>
2022-03-05 13:51:02 -0300

    changes

--- src/rawhandler/index.ts
+++ src/rawhandler/index.ts
@@ -41,7 +41,7 @@ async function handleChapter(images_array: string[], number: string, title: stri
         await exec(`python3 src/rawhandler/SmartStitchConsole.py -i "${directory}" -H 12000 -cw 800 -w 2 -t ".jpeg" -s 90`);
         console.log('All images have been stitched.')
 
-        await exec(`./waifu2x-ncnn-vulkan -n 3 -s 1 -o ../../${waifu_directory}/ -i ../../${directory}/Stitched -f jpg -j 3:3:3`, { cwd: waifu })
+        await exec(`./waifu2x-ncnn-vulkan -n 3 -s 1 -o ../../${waifu_directory}/ -i ../../${directory}/Stitched -f jpg -j 2:2:2`, { cwd: waifu })
         console.log('All images have been through waifu-2x-caffe.')
 
         await exec(`7z a ${chaptername}.7z  ./${waifu_directory}/*`)
