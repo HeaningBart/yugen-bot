0aeb7954911f955055acbd97ab0ab4f611670e35
Gustavo Souzavs <gustavo.araujo@aluno.uepa.br>
2022-08-01 11:29:07 -0300

    fixed gdrive process

--- src/rawhandler/index.ts
+++ src/rawhandler/index.ts
@@ -710,7 +710,7 @@ export async function processNaver(url: string, channel_name: string) {
                 return `${name}.7z`;
             }
         } else if (url.includes('drive.google')) {
-            const file_id = url.replaceAll('https://drive.google.com/file/d/', '').replaceAll('/view?usp=sharing', '').replaceAll('/view', '');
+            const file_id = url.replaceAll('https://drive.google.com/file/d/', '').replaceAll('/view?usp=sharing', '').replaceAll('/view?usp=drivesdk', '').replaceAll('/view', '');
             const file_url = `https://drive.google.com/uc?export=download&id=${file_id}&confirm=t`;
             await download(file_url, `./${directory}`);
             const files = await fs.readdir(`./${directory}`);
